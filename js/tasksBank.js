83cd7c90b984f2b196e05df0a5e9502440da68cd
MMadejsza <maciej.madejsza@gmail.com>
2022-11-27 21:15:40 +0000

    phantom tag in html and debuggers removed

--- js/tasksBank.js
+++ js/tasksBank.js
@@ -1,10 +1,6 @@
 class TasksBank {
 	constructor(task, ulInDOM, counterPlaceInDOM) {
 		this.tasksList = [];
-		console.log(
-			'🚀 ~ file: tasksBank.js ~ line 4 ~ TasksBank ~ constructor ~ this.tasksList at start:',
-			this.tasksList
-		);
 		this.task = task;
 		this.ulInDOM = ulInDOM;
 		this.counterPlaceInDOM = counterPlaceInDOM;
@@ -21,8 +17,6 @@ class TasksBank {
 			li.querySelector('button').addEventListener('click', that.removeTask);
 			this.tasksList.push(li);
 			this.task.value = '';
-			console.log(li);
-			console.log(this.tasksList);
 			//- document.querySelector('li button').addEventListener('click', removeTask); will not work because for now "li" isn't in DOM yet
 			this.tasksOrganiser.indexTasks(this.tasksList);
 		} else {
@@ -35,19 +29,10 @@ class TasksBank {
 	}
 	removeTask = (e) => {
 		//- button's parent removed from DOM:
-		console.log(
-			'🚀 ~ file: tasksBank.js ~ line 4 ~ TasksBank ~ constructor ~ this.tasksList before removing:',
-			this.tasksList
-		);
-		console.log('e.target.parentNode', e.target.parentNode);
 		e.target.parentNode.remove();
 		//- dataset previously given in "indexTasks()"
 		let elementID = e.target.parentNode.dataset.id;
 		this.tasksList.splice(elementID, 1);
-		console.log(
-			'🚀 ~ file: tasksBank.js ~ line 4 ~ TasksBank ~ constructor ~ this.tasksList after removing:',
-			this.tasksList
-		);
 		this.tasksOrganiser.indexTasks(this.tasksList);
 		this.tasksOrganiser.countTasks(this.counterPlaceInDOM);
 	};
