f7c897f5ca8dbb4d1770aed7f8434b9dd693c944
MMadejsza <maciej.madejsza@gmail.com>
2022-11-27 16:55:47 +0000

    start class structure created

--- js/tasksOrganiser.js
+++ js/tasksOrganiser.js
@@ -1,1 +1,10 @@
-console.log('taskfilter');
+class tasksOrganiser extends TasksBank {
+	constructor(event, task, ulInDOM, tasksList, counterPlaceInDOM) {
+		super(event, task, ulInDOM, tasksList);
+		this.counterPlaceInDOM = counterPlaceInDOM;
+	}
+	countTasks() {}
+	indexTasks() {}
+	displayFilteredTasks() {}
+	filterTasks(event) {}
+}
