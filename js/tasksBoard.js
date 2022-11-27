b7726c213a70e932c88d3f86328928a2a6a2b7a7
MMadejsza <maciej.madejsza@gmail.com>
2022-11-27 19:52:35 +0000

    TasksBank " e" removed

--- js/tasksBoard.js
+++ js/tasksBoard.js
@@ -5,13 +5,13 @@ class TasksBoard {
 		this.btnAddTask = document.querySelector('button');
 		this.ul = document.querySelector('ul');
 		this.counterPlace = document.querySelector('h1 span');
-		this.tasksBank = new TasksBank(e, this.inputTask, this.ul, this.counterPlace);
+		this.tasksBank = new TasksBank(this.inputTask, this.ul, this.counterPlace);
 		this.tasksOrganiser = new tasksOrganiser();
 		this.btnAddTask.addEventListener('click', this.processTask.bind(this));
 		this.inputSearch.addEventListener('input', this.searchTasks.bind(this));
 		this.render();
 	}
-	processTask() {
+	processTask(e) {
 		this.tasksBank.addTask(e);
 		this.tasksBank.showAllTasks();
 	}
