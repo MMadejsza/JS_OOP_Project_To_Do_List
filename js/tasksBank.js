3689e80810ee6d97e9344bb5a0c62d5270910e1d
MMadejsza <maciej.madejsza@gmail.com>
2022-11-27 23:06:10 +0000

    code optimalization

--- js/tasksBank.js
+++ js/tasksBank.js
@@ -19,16 +19,16 @@ class TasksBank {
 			li.setAttribute('class', 'LiTask');
 			li.innerHTML = this.task.value + ' <button class="btnRemove">Done</button>';
 			li.querySelector('button').addEventListener('click', that.removeTask);
-			this.get_tasksList().push(li);
+			this._tasksList.push(li);
 			this.task.value = '';
 			//- document.querySelector('li button').addEventListener('click', removeTask); will not work because for now "li" isn't in DOM yet
-			this.tasksOrganiser.indexTasks(this.get_tasksList());
+			this.tasksOrganiser.indexTasks();
 		} else {
 			alert('No task given');
 		}
 	}
 	showAllTasks() {
-		this.get_tasksList().forEach((taskAsLi) => this.ulInDOM.appendChild(taskAsLi));
+		this._tasksList.forEach((taskAsLi) => this.ulInDOM.appendChild(taskAsLi));
 		this.tasksOrganiser.countTasks(this.counterPlaceInDOM);
 	}
 	removeTask = (e) => {
@@ -36,8 +36,8 @@ class TasksBank {
 		e.target.parentNode.remove();
 		//- dataset previously given in "indexTasks()"
 		let elementID = e.target.parentNode.dataset.id;
-		this.get_tasksList().splice(elementID, 1);
-		this.tasksOrganiser.indexTasks(this.get_tasksList());
+		this._tasksList.splice(elementID, 1);
+		this.tasksOrganiser.indexTasks();
 		this.tasksOrganiser.countTasks(this.counterPlaceInDOM);
 	};
 }
