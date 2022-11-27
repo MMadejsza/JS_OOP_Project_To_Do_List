ecb4e2f55a63ec9940b993c5c75564567afe91b7
MMadejsza <maciej.madejsza@gmail.com>
2022-11-27 15:58:36 +0000

    addTask() created

--- js/_tasksBank.js
+++ js/_tasksBank.js
@@ -1,7 +1,24 @@
 class TaskBank {
-	constructor() {}
-
-	addTask() {}
+	constructor(e, task) {
+		this.tasksList = [];
+		this.task = task;
+	}
+	addTask(e) {
+		const that = this;
+		e.preventDefault();
+		if (this.task) {
+			//- if isn't empty string:
+			const li = document.createElement('li');
+			li.setAttribute('class', 'LiTask');
+			li.innerHTML = this.task + ' <button>Done</button>';
+			tasksList.push(li);
+			// document.querySelector('li button').addEventListener('click', removeTask); will not work because for now "li" isn't in DOM yet
+			li.querySelector('button').addEventListener('click', that.removeTask);
+			//! indexElements(); Filter(this.tasksList).indexElements
+		} else {
+			alert('No task given');
+		}
+	}
 	removeTask() {}
 	showAllTasks() {}
 }
