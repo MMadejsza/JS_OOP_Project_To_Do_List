f7733ad4106ab3a3f2de860f4fb70fc6f0380dc7
MMadejsza <maciej.madejsza@gmail.com>
2022-11-27 19:27:09 +0000

    class start structure - assigned DOM

--- js/tasksBoard.js
+++ js/tasksBoard.js
@@ -1,5 +1,15 @@
 class TasksBoard {
-	constructor() {}
+	constructor() {
+		this.inputTask = document.querySelector('.task');
+		this.inputSearch = document.querySelector('input:nth-child(3)');
+		this.btnAddTask = document.querySelector('button');
+		this.ul = document.querySelector('ul');
+		this.counterPlace = document.querySelector('h1 span');
+		this.tasksBank = new TasksBank(e, this.inputTask, this.ul, this.counterPlace);
+		this.btnAddTask.addEventListener('click', this.processTask.bind(this));
+		this.inputSearch.addEventListener('input', this.searchTasks.bind(this));
+		this.render();
+	}
 	processTask() {}
 	searchTasks() {}
 	render() {}
